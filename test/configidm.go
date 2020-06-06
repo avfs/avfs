46e1d5694d05d6ab66fc507c749497a91842bf81
psadac <paulsadac@gmail.com>
2020-06-06 14:53:54 +0200

    Test : Features -> hasFeatures

--- test/configidm.go
+++ test/configidm.go
@@ -34,7 +34,7 @@ type ConfigIdm struct {
 func NewConfigIdm(t *testing.T, idm avfs.IdentityMgr) *ConfigIdm {
 	ci := &ConfigIdm{t: t, idm: idm}
 
-	ci.cantTest = idm.Features(avfs.FeatIdentityMgr)
+	ci.cantTest = idm.HasFeatures(avfs.FeatIdentityMgr)
 
 	if ci.cantTest {
 		CreateGroups(t, idm, "")
