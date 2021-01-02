fa41bc726e62af0049963cc32393378cac953c62
psadac <paulsadac@gmail.com>
2021-01-02 12:46:09 +0100

    test: Add Windows tests for Clean() function

--- test/path.go
+++ test/path.go
@@ -229,62 +229,86 @@ func (sfs *SuiteFS) Clean() {
 	t, _, removeDir := sfs.CreateRootDir(UsrTest)
 	defer removeDir()
 
-	cleantests := []*pathTest{
-		// Already clean
-		{"abc", "abc"},
-		{"abc/def", "abc/def"},
-		{"a/b/c", "a/b/c"},
-		{".", "."},
-		{"..", ".."},
-		{"../..", "../.."},
-		{"../../abc", "../../abc"},
-		{"/abc", "/abc"},
-		{"/", "/"},
-
-		// Empty is current dir
-		{"", "."},
-
-		// Remove trailing slash
-		{"abc/", "abc"},
-		{"abc/def/", "abc/def"},
-		{"a/b/c/", "a/b/c"},
-		{"./", "."},
-		{"../", ".."},
-		{"../../", "../.."},
-		{"/abc/", "/abc"},
-
-		// Remove doubled slash
-		{"abc//def//ghi", "abc/def/ghi"},
-		{"//abc", "/abc"},
-		{"///abc", "/abc"},
-		{"//abc//", "/abc"},
-		{"abc//", "abc"},
-
-		// Remove . elements
-		{"abc/./def", "abc/def"},
-		{"/./abc/def", "/abc/def"},
-		{"abc/.", "abc"},
-
-		// Remove .. elements
-		{"abc/def/ghi/../jkl", "abc/def/jkl"},
-		{"abc/def/../ghi/../jkl", "abc/jkl"},
-		{"abc/def/..", "abc"},
-		{"abc/def/../..", "."},
-		{"/abc/def/../..", "/"},
-		{"abc/def/../../..", ".."},
-		{"/abc/def/../../..", "/"},
-		{"abc/def/../../../ghi/jkl/../../../mno", "../../mno"},
-		{"/../abc", "/abc"},
-
-		// Combinations
-		{"abc/./../def", "def"},
-		{"abc//./../def", "def"},
-		{"abc/../../././../def", "../../def"},
-	}
-
 	vfs := sfs.GetFsRead()
 
-	for _, test := range cleantests {
+	var cleanTests []*pathTest
+
+	switch vfs.OSType() {
+	case avfs.OsWindows:
+		cleanTests = []*pathTest{
+			{`c:`, `c:.`},
+			{`c:\`, `c:\`},
+			{`c:\abc`, `c:\abc`},
+			{`c:abc\..\..\.\.\..\def`, `c:..\..\def`},
+			{`c:\abc\def\..\..`, `c:\`},
+			{`c:\..\abc`, `c:\abc`},
+			{`c:..\abc`, `c:..\abc`},
+			{`\`, `\`},
+			{`/`, `\`},
+			{`\\i\..\c$`, `\c$`},
+			{`\\i\..\i\c$`, `\i\c$`},
+			{`\\i\..\I\c$`, `\I\c$`},
+			{`\\host\share\foo\..\bar`, `\\host\share\bar`},
+			{`//host/share/foo/../baz`, `\\host\share\baz`},
+			{`\\a\b\..\c`, `\\a\b\c`},
+			{`\\a\b`, `\\a\b`},
+		}
+	default:
+		cleanTests = []*pathTest{
+			// Already clean
+			{"abc", "abc"},
+			{"abc/def", "abc/def"},
+			{"a/b/c", "a/b/c"},
+			{".", "."},
+			{"..", ".."},
+			{"../..", "../.."},
+			{"../../abc", "../../abc"},
+			{"/abc", "/abc"},
+			{"/", "/"},
+
+			// Empty is current dir
+			{"", "."},
+
+			// Remove trailing slash
+			{"abc/", "abc"},
+			{"abc/def/", "abc/def"},
+			{"a/b/c/", "a/b/c"},
+			{"./", "."},
+			{"../", ".."},
+			{"../../", "../.."},
+			{"/abc/", "/abc"},
+
+			// Remove doubled slash
+			{"abc//def//ghi", "abc/def/ghi"},
+			{"//abc", "/abc"},
+			{"///abc", "/abc"},
+			{"//abc//", "/abc"},
+			{"abc//", "abc"},
+
+			// Remove . elements
+			{"abc/./def", "abc/def"},
+			{"/./abc/def", "/abc/def"},
+			{"abc/.", "abc"},
+
+			// Remove .. elements
+			{"abc/def/ghi/../jkl", "abc/def/jkl"},
+			{"abc/def/../ghi/../jkl", "abc/jkl"},
+			{"abc/def/..", "abc"},
+			{"abc/def/../..", "."},
+			{"/abc/def/../..", "/"},
+			{"abc/def/../../..", ".."},
+			{"/abc/def/../../..", "/"},
+			{"abc/def/../../../ghi/jkl/../../../mno", "../../mno"},
+			{"/../abc", "/abc"},
+
+			// Combinations
+			{"abc/./../def", "def"},
+			{"abc//./../def", "def"},
+			{"abc/../../././../def", "../../def"},
+		}
+	}
+
+	for _, test := range cleanTests {
 		s := vfs.Clean(test.path)
 		if s != test.result {
 			t.Errorf("Clean(%q) = %q, want %q", test.path, s, test.result)
