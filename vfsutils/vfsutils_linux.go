fcb2df4edf17693bd986b62170d2974399d09fd9
psadac <paulsadac@gmail.com>
2021-01-27 23:16:28 +0100

    vdsutils: Remove unused function IsLinuxWSL

--- vfsutils/vfsutils_linux.go
+++ vfsutils/vfsutils_linux.go
@@ -19,7 +19,6 @@
 package vfsutils
 
 import (
-	"bytes"
 	"math"
 	"os"
 	"sync"
@@ -77,24 +76,4 @@ func AsStatT(value interface{}) *avfs.StatT {
 	default:
 		return &avfs.StatT{Uid: math.MaxUint32, Gid: math.MaxUint32}
 	}
-}
-
-// IsLinuxWSL indicates if the current operating system is Windows Subsystem for Linux.
-func IsLinuxWSL() bool {
-	var uname syscall.Utsname
-	if err := syscall.Uname(&uname); err != nil {
-		return false
-	}
-
-	buf := make([]byte, 0, len(uname.Release))
-
-	for _, c := range uname.Release {
-		if c == 0 {
-			break
-		}
-
-		buf = append(buf, byte(c))
-	}
-
-	return bytes.Contains(buf, []byte("microsoft"))
 }
