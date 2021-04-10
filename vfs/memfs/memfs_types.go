854c39bee671b6bfef872907627fdc1545f01578
psadac <paulsadac@gmail.com>
2021-04-10 18:43:09 +0200

    memfs: Add comments for types

--- vfs/memfs/memfs_types.go
+++ vfs/memfs/memfs_types.go
@@ -31,65 +31,32 @@ const (
 
 // MemFS implements a memory file system using the avfs.VFS interface.
 type MemFS struct {
-	// rootNode represent the root directory of the file system.
-	rootNode *dirNode
-
-	// memAttrs represents the file system attributes.
-	memAttrs *memAttrs
-
-	// User is the current user of the file system.
-	user avfs.UserReader
-
-	// curDir is the current directory.
-	curDir string
+	rootNode *dirNode        // rootNode represent the root directory of the file system.
+	memAttrs *memAttrs       // memAttrs represents the file system attributes.
+	user     avfs.UserReader // User is the current user of the file system.
+	curDir   string          // curDir is the current directory.
 }
 
 // memAttrs represents the file system attributes for MemFS.
 type memAttrs struct {
-	// idm is the identity manager of the file system.
-	idm avfs.IdentityMgr
-
-	// name is the name of the file system.
-	name string
-
-	// feature defines the list of features available on this file system.
-	feature avfs.Feature
-
-	// lastId is the last unique id used to identify files uniquely.
-	lastId uint64
-
-	// umask is the user file creation mode mask.
-	umask int32
+	idm     avfs.IdentityMgr // idm is the identity manager of the file system.
+	name    string           // name is the name of the file system.
+	feature avfs.Feature     // feature defines the list of features available on this file system.
+	lastId  uint64           // lastId is the last unique id used to identify files uniquely.
+	umask   int32            // umask is the user file creation mode mask.
 }
 
 // MemFile represents an open file descriptor.
 type MemFile struct {
-	// nd is node of the file.
-	nd node
-
-	// memFS is the memory file system of the file.
-	memFS *MemFS
-
-	// name is the name of the file.
-	name string
-
-	// dirInfos stores the files informations returned by Readdir function.
-	dirInfos []os.FileInfo
-
-	// dirNames stores the names of the file returned by Readdirnames function.
-	dirNames []string
-
-	// at is current position in the file used by Read and Write functions.
-	at int64
-
-	// dirIndex is the position of the current index for dirInfos ou dirNames slices.
-	dirIndex int
-
-	// mu is the RWMutex used to prevent concurrent u
-	mu sync.RWMutex
-
-	// wantMode
-	wantMode avfs.WantMode
+	nd       node          // nd is node of the file.
+	memFS    *MemFS        // memFS is the memory file system of the file.
+	name     string        // name is the name of the file.
+	dirInfos []os.FileInfo // dirInfos stores the files informations returned by Readdir function.
+	dirNames []string      // dirNames stores the names of the file returned by Readdirnames function.
+	at       int64         // at is current position in the file used by Read and Write functions.
+	dirIndex int           // dirIndex is the position of the current index for dirInfos ou dirNames slices.
+	mu       sync.RWMutex  // mu is the RWMutex used to access content of MemFile.
+	wantMode avfs.WantMode // wantMode
 }
 
 // Option defines the option function used for initializing MemFS.
@@ -118,11 +85,8 @@ type node interface {
 
 // dirNode is the structure for a directory.
 type dirNode struct {
-	// children are the nodes present in the directory.
-	children children
-
-	// baseNode is the common structure of directories, files and symbolic links.
-	baseNode
+	children children // children are the nodes present in the directory.
+	baseNode          // baseNode is the common structure of directories, files and symbolic links.
 }
 
 // children are the children of a directory.
@@ -130,70 +94,46 @@ type children = map[string]node
 
 // fileNode is the structure for a file.
 type fileNode struct {
-	// data is the file content.
-	data []byte
-
-	// baseNode is the common structure of directories, files and symbolic links.
-	baseNode
-
-	// id is a unique id to identify a file (used by SameFile function).
-	id uint64
-
-	// nlink os the number of hardlinks to this file.
-	nlink int
+	data     []byte // data is the file content.
+	baseNode        // baseNode is the common structure of directories, files and symbolic links.
+	id       uint64 // id is a unique id to identify a file (used by SameFile function).
+	nlink    int    // nlink is the number of hardlinks to this fileNode.
 }
 
 // symlinkNode is the structure for a symbolic link.
 type symlinkNode struct {
-	// link is the symbolic link value.
-	link string
-
-	// baseNode is the common structure of directories, files and symbolic links.
-	baseNode
+	link     string // link is the symbolic link value.
+	baseNode        // baseNode is the common structure of directories, files and symbolic links.
 }
 
 // baseNode is the common structure of directories, files and symbolic links.
 type baseNode struct {
-	// mu is the RWMutex used to access the content of the node.
-	mu sync.RWMutex
-
-	// mtime is the modification time.
-	mtime int64
-
-	// mode represents a file's mode and permission bits.
-	mode os.FileMode
-
-	// uid is the user id.
-	uid int
-
-	// gid is the group id.
-	gid int
+	mu    sync.RWMutex // mu is the RWMutex used to access the content of the node.
+	mtime int64        // mtime is the modification time.
+	mode  os.FileMode  // mode represents a file's mode and permission bits.
+	uid   int          // uid is the user id.
+	gid   int          // gid is the group id.
 }
 
 // slMode defines the behavior of searchNode function relatively to symlinks.
 type slMode int
 
 const (
-	// slmLstat makes searchNode function follow symbolic links like Lstat.
-	slmLstat slMode = iota + 1
-
-	// slmStat makes searchNode function follow symbolic links like Stat.
-	slmStat
-
-	// slmEval makes searchNode function follow symbolic links like EvalSymlink.
-	slmEval
+	slmLstat slMode = iota + 1 // slmLstat makes searchNode function follow symbolic links like Lstat.
+	slmStat                    // slmStat makes searchNode function follow symbolic links like Stat.
+	slmEval                    // slmEval makes searchNode function follow symbolic links like EvalSymlink.
 )
 
 // fStat is the implementation of os.FileInfo returned by Stat and Lstat.
 type fStat struct {
-	name  string
-	id    uint64
-	size  int64
-	mtime int64
-	uid   int
-	gid   int
-	nlink int
-	mode  os.FileMode
+	name  string      // name is the name of the file.
+	id    uint64      // id is a unique id to identify a file (used by SameFile function).
+	size  int64       // size is the size of the file.
+	mtime int64       // mtime is the modification time.
+	uid   int         // uid is the user id.
+	gid   int         // gid is the group id.
+	nlink int         // nlink is the number of hardlinks to this fileNode.
+	mode  os.FileMode // mode represents a file's mode and permission bits.
 }
 
 // removeStack is a stack of directories to be removed during tree traversal in RemoveAll function.
