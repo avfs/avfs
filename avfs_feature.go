54abc7d35815f9b06fe80ac632e6dd49303f69cc
psadac <paulsadac@gmail.com>
2021-05-20 12:53:30 +0200

    avfs: Update custom Stringer function

--- avfs_feature.go
+++ avfs_feature.go
@@ -39,7 +39,7 @@ func (i Feature) String() string {
 	}
 	sb := make([]byte, 0, len(_Feature_name)/2)
 	sb = append(sb, []byte("Feature(")...)
-	for mask := Feature(1); mask <= i; mask <<= 1 {
+	for mask := Feature(1); mask > 0 && mask <= i; mask <<= 1 {
 		val := i & mask
 		if val == 0 {
 			continue
