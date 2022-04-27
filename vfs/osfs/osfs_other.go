8ece532c4f66d68a217ff703f9e675fe23e11949
psadac <paulsadac@gmail.com>
2022-04-28 01:00:28 +0200

    osfs: Fix build for other operating systems (not Linux or Windows)

---
+++ vfs/osfs/osfs_other.go
@@ -0,0 +1,72 @@
+//
+//  Copyright 2022 The AVFS authors
+//
+//  Licensed under the Apache License, Version 2.0 (the "License");
+//  you may not use this file except in compliance with the License.
+//  You may obtain a copy of the License at
+//
+//  	http://www.apache.org/licenses/LICENSE-2.0
+//
+//  Unless required by applicable law or agreed to in writing, software
+//  distributed under the License is distributed on an "AS IS" BASIS,
+//  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+//  See the License for the specific language governing permissions and
+//  limitations under the License.
+//
+
+//go:build !linux && !windows
+
+package osfs
+
+import (
+	"io/fs"
+
+	"github.com/avfs/avfs"
+)
+
+// Chroot changes the root to that specified in path.
+// If there is an error, it will be of type *PathError.
+func (vfs *OsFS) Chroot(path string) error {
+	const op = "chroot"
+
+	return &fs.PathError{Op: op, Path: path, Err: avfs.ErrOpNotPermitted}
+}
+
+// SetUser sets and returns the current user.
+// If the user is not found, the returned error is of type UnknownUserError.
+func SetUser(name string) (avfs.UserReader, error) {
+	return nil, avfs.ErrPermDenied
+}
+
+// ToSysStat takes a value from fs.FileInfo.Sys() and returns a value that implements interface avfs.SysStater.
+func (vfs *OsFS) ToSysStat(info fs.FileInfo) avfs.SysStater {
+	u := avfs.CurrentUser
+
+	return &OtherSysStat{gid: u.Gid(), uid: u.Gid()}
+}
+
+// User returns the current user of the OS.
+func User() avfs.UserReader {
+	return avfs.CurrentUser
+}
+
+// OtherSysStat implements SysStater interface returned by fs.FileInfo.Sys() for non Linux/Windows file system.
+type OtherSysStat struct {
+	gid int
+	uid int
+}
+
+// Gid returns the group id.
+func (oss *OtherSysStat) Gid() int {
+	return oss.gid
+}
+
+// Uid returns the user id.
+func (oss *OtherSysStat) Uid() int {
+	return oss.uid
+}
+
+// Nlink returns the number of hard links.
+func (oss *OtherSysStat) Nlink() uint64 {
+	return 1
+}
