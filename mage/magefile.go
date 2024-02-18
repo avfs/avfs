29f064c141914c0e62446c50648e032cd3cb06dc
psadac <paulsadac@gmail.com>
2024-02-18 14:03:05 +0100

    mage: Use env to preserve path

--- mage/magefile.go
+++ mage/magefile.go
@@ -194,7 +194,7 @@ func sudo(cmd string, args ...string) error {
 		return sh.RunV(cmd, args...)
 	}
 
-	sudoArgs := append([]string{"-n", "-E", "PATH=$PATH", cmd}, args...)
+	sudoArgs := append([]string{"-n", "-E", "env", "PATH=$PATH", cmd}, args...)
 
 	return sh.RunV(sudoCmd, sudoArgs...)
 }
