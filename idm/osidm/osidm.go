b39d668e15656054234066ee17da07fed0a57153
Paul Sadac <paulsadac@gmail.com>
2025-08-13 09:21:48 +0200

    Update osidm.go with new content

--- idm/osidm/osidm.go
+++ idm/osidm/osidm.go
@@ -1,18 +1,16 @@
-//
 //  Copyright 2020 The AVFS authors
-//
+//  
 //  Licensed under the Apache License, Version 2.0 (the "License");
 //  you may not use this file except in compliance with the License.
 //  You may obtain a copy of the License at
-//
-//  	http://www.apache.org/licenses/LICENSE-2.0
-//
+//  
+//  \thttp://www.apache.org/licenses/LICENSE-2.0
+//  
 //  Unless required by applicable law or agreed to in writing, software
 //  distributed under the License is distributed on an "AS IS" BASIS,
 //  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 //  See the License for the specific language governing permissions and
 //  limitations under the License.
-//
 
 // Package osidm implements an identity manager using os functions.
 //
@@ -82,6 +80,7 @@ func run(cmd string, args ...string) error {
 	c := exec.Command(cmd, args...)
 
 	var stderr bytes.Buffer
+
 	c.Stderr = &stderr
 
 	err := c.Run()
@@ -101,6 +100,7 @@ func output(cmd string, args ...string) (string, error) {
 	c := exec.Command(cmd, args...)
 
 	var stderr bytes.Buffer
+
 	c.Stderr = &stderr
 
 	buf, err := c.Output()
